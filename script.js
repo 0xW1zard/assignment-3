30e6c4f68fa099c985947aab1d1e88464bdacc02
Himel Ahmed <ahimel963@gmail.com>
2026-02-24 10:52:51 +0600

    top button state

---
+++ script.js
@@ -0,0 +1,33 @@
+let interviewList = [];
+let rejcetList = [];
+
+let total = document.getElementById('total-counter');
+let interCount = document.getElementById('inter-counter')
+let rejCount = document.getElementById('reject-counter')
+
+const allButton = document.getElementById('all-btn')
+const interviewButton = document.getElementById('inter-btn')
+const rejcetButton = document.getElementById('rej-btn')
+
+const mainContainer = document.getElementById('main-container');
+
+function calculateCount(){
+    total.innerText = mainContainer.children.length;
+    interCount.innerText = interviewList.length;
+    rejCount.innerText = rejcetList.length;
+}
+
+calculateCount();
+
+function buttonToggle(id){
+    allButton.classList.add('btn-soft')
+    interviewButton.classList.add('btn-soft')
+    rejcetButton.classList.add('btn-soft')
+
+  
+
+    const select = document.getElementById(id)
+    console.log(select)
+    select.classList.remove('btn-soft')
+    select.classList.add('btn-primary')
+}
